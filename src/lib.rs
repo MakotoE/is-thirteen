f651a5c50fddb1ec14a97c065d2b9738cc58d294
Makoto <makoto@emurasoft.com>
2021-07-31 21:29:15 -0700

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -85,6 +85,23 @@ mod tests {
     #[case("xiii", true)] // 5
     #[case("IIIIIIIIIIIII", true)] // 6
     #[case("IlIlIlIlIlIlI", true)] // 7
+    #[case("https://en.wikipedia.org/wiki/This_Is_Thirteen", true)] // 8
+    #[case("https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/s320x320/e35/12237511_444845689040315_1101385461_n.jpg", true)] // 9
+    #[case("http://www.metal-archives.com/images/1/5/3/7/153772.jpg", true)] // 10
+    #[case("https://www.youtube.com/watch?v=pte3Jg-2Ax4", true)] // 11
+    #[case("https://www.youtube.com/watch?v=33Kv5D2zwyc", true)] // 12
+    #[case("thirteen", true)] // 13
+    #[case("Thirteen", true)] // 14
+    #[case("Remy Hadley", true)] // 15
+    #[case("Olivia Wilde", true)] // 16
+    #[case("weedle", true)] // 17
+    #[case("baker's dozen", true)] // 18
+    #[case("Dr. Remy Beauregard Hadley", true)] // 19
+    #[case("Patty Tsai", true)] // 20
+    #[case("PT", true)] // 21
+    #[case("Washington Luís", true)] // 22
+    #[case("Millard Fillmore", true)] // 23
+    #[case("https://en.wikipedia.org/wiki/XIII_(video_game)", true)] // 24
     // Added test cases
     #[case(0, false)]
     #[case(13.0, true)]
